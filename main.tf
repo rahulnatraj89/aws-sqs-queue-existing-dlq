3122a29d6b712724c95e277920f675b9a8e36672
rahulnatraj89 <122457791+rahulnatraj89@users.noreply.github.com>
2023-02-01 20:55:53 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,136 @@
+###sqs queue with existing dead-letter-queue attach code
+
+resource "aws_sqs_queue" "queue" {
+  name                              = "${var.name}${var.fifo_queue == true ? ".fifo" : ""}"
+  message_retention_seconds         = var.message_retention_seconds
+  visibility_timeout_seconds        = var.visibility_timeout_seconds
+  fifo_queue                        = var.fifo_queue
+  content_based_deduplication       = var.content_based_deduplication
+  kms_master_key_id                 = var.kms_master_key_id_sqs
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+  max_message_size                  = var.max_message_size
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn,#########(existing dead-letter-queue-arn)
+    maxReceiveCount     = 10
+  })
+  tags = var.tags
+}
+resource "aws_sqs_queue_policy" "queue" {
+  queue_url = aws_sqs_queue.queue.id
+  policy    = data.aws_iam_policy_document.queue.json
+}
+##################################################################
+(aws user and IAM policy jasoncode as variable"policy_queue")
+###################################################################
+
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect    = "Allow"
+    resources = [aws_sqs_queue.queue.arn]
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ListQueueTags",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = var.allowed_arns == null ? [local.account_id] : var.allowed_arns
+    }
+  }
+}
+
+
+
+######################################################
+create a sqs-queue with new dlq-attach policy
+######################################################
+
+
+resource "aws_sqs_queue" "queue" {
+  name                              = "${var.name}${var.fifo_queue == true ? ".fifo" : ""}"
+  message_retention_seconds         = var.message_retention_seconds
+  visibility_timeout_seconds        = var.visibility_timeout_seconds
+  fifo_queue                        = var.fifo_queue
+  content_based_deduplication       = var.content_based_deduplication
+  kms_master_key_id                 = var.kms_master_key_id_sqs
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+  max_message_size                  = var.max_message_size
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn,
+    maxReceiveCount     = 10
+  })
+  tags = var.tags
+}
+
+resource "aws_sqs_queue_policy" "queue" {
+  queue_url = aws_sqs_queue.queue.id
+  policy    = data.aws_iam_policy_document.queue.json
+}
+##################################################################
+(aws user and IAM policy jasoncode as variable"policy_queue")
+###################################################################
+
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect    = "Allow"
+    resources = [aws_sqs_queue.queue.arn]
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ListQueueTags",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = var.allowed_arns == null ? [local.account_id] : var.allowed_arns
+    }
+  }
+}
+
+resource "aws_sqs_queue" "deadletter_queue" {
+  name                              = "${var.name}-dead-letter-queue${var.fifo_queue == true ? ".fifo" : ""}"
+  message_retention_seconds         = var.message_retention_seconds
+  visibility_timeout_seconds        = var.visibility_timeout_seconds
+  fifo_queue                        = var.fifo_queue
+  content_based_deduplication       = var.content_based_deduplication
+  kms_master_key_id                 = var.kms_master_key_id
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+  max_message_size                  = var.max_message_size
+  tags                              = var.tags
+}
+
+resource "aws_sqs_queue_policy" "deadletter_queue" {
+  queue_url = aws_sqs_queue.deadletter_queue.id
+  policy    = data.aws_iam_policy_document.deadletter_queue.json
+}
+
+##################################################################
+(aws user and IAM policy jasoncode as variable"deadletter_queue")
+###################################################################
+
+data "aws_iam_policy_document" "deadletter_queue" {
+  statement {
+    effect    = "Allow"
+    resources = [aws_sqs_queue.deadletter_queue.arn]
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ListQueueTags",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = var.allowed_arns == null ? [local.account_id] : var.allowed_arns
+    }
+  }
+}
